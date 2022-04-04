0446159bfed4b590b7aef37cf495b01af8ac99fc
DIFA <42770277+green404@users.noreply.github.com>
2022-04-05 01:03:56 +0700

    Update UMIWAB.js

--- UMIWAB.js
+++ UMIWAB.js
@@ -7,10 +7,6 @@
 			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-en.html"
 		},
 		{
-			"languaje": "es",
-			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-es.html"
-		},
-		{
 			"languaje": "in",
 			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-in.html"
 		}
