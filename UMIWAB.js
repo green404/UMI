2357a325423b2b8e744fcc333e2df7ca3d9070cf
DIFA <42770277+green404@users.noreply.github.com>
2022-04-06 21:12:38 +0700

    Add files via upload

---
+++ UMIWAB.js
@@ -0,0 +1,18 @@
+{
+	"version": "14.0",
+	"Hubungi Admin": "https://wa.me/6281380013449",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-es.html"
+		},
+		{
+			"languaje": "in",
+			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-in.html"
+		}
+	]
+}
